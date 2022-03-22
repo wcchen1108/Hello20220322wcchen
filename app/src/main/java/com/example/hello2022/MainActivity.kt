60b080e7db8ec1741ec400912a1d5c125653f11e
wcchen <s1091759@gm.pu.edu.tw>
2022-03-22 09:49:16 +0800

    Second Git Commit

--- app/src/main/java/com/example/hello2022/MainActivity.kt
+++ app/src/main/java/com/example/hello2022/MainActivity.kt
@@ -2,10 +2,13 @@ package com.example.hello2022
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var txv: TextView = findViewById<TextView>(R.id.txv)
+        txv.text = "Hello wcchen"
     }
 }
