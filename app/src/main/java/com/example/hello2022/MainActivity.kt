eee95a6de4d934bb4e6cfe6f973ca90e2fcd87f1
wcchen <s1091759@gm.pu.edu.tw>
2022-03-22 10:28:26 +0800

    Add "Master" Code

--- app/src/main/java/com/example/hello2022/MainActivity.kt
+++ app/src/main/java/com/example/hello2022/MainActivity.kt
@@ -10,5 +10,11 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         var txv: TextView = findViewById<TextView>(R.id.txv)
         txv.text = "Hello wcchen"
+        TestMaster()
     }
+    fun TestMaster(){
+        var txv:TextView = findViewById<TextView>(R.id.txv)
+        txv.text = "Master分支"
+    }
+
 }
